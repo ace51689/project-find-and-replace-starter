81625b75410771f4c73202b9ea54fbde1da7acd8
Michael Vitollo <ace51689@gmail.com>
2020-06-08 23:40:13 -0400

    Ver 1.0 all working except the .replace portion

--- main.js
+++ main.js
@@ -8,20 +8,41 @@ const replaceAllButton = document.querySelector(".replace-all-button")
 // The following variable holds your OUTER ARRAY of row elements.
 // Later you will need an OUTER LOOP to loop over the individual elements within
 // this array.
-const rowElements = document.querySelectorAll(".row")
 
-// When you call the function belwo, it will get and return an INNER ARRAY
+const rowElements = document.querySelectorAll(".row")
+// console.log(rowElements)
+
+// When you call the function below, it will get and return an INNER ARRAY
 // containing the cell elements for a given row.
 // Call this function from WITHIN your row elements loop. Then you will, in turn,
 // need to loop over the resulting cell elements. But where should this whole
 // NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
-function getCellElements (currentRowElement) {
+
+function getCellElements(currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
 }
 
 
 // YOUR CODE GOES HERE
+replaceAllButton.addEventListener('click', function () {
+    let findInputValue = findInput.value.toString()
+    let replaceInputValue = replaceInput.value.toString()
+    // console.log(findInputValue, replaceInputValue)
+    for (let increment = 0; increment < rowElements.length; increment += 1) {
+        let result = getCellElements(rowElements[increment])
+        // console.log(result)
+        for (let count = 0; count < result.length; count += 1) {
+            let cellElement = result[count].innerHTML
+            // console.log(cellElement)
+            // console.log(cellElement.includes(findInputValue))
+            if (cellElement.includes(findInputValue)) {
+                cellElement.replace(findInputValue, replaceInputValue) //I got this far and couldn't get this to work. Not sure how else to go about it.
+            } else {
 
+            }
+        }
+    }
+})
 
 // One last thing: dedicate very careful attention to using variables and
 // naming them accurately.
